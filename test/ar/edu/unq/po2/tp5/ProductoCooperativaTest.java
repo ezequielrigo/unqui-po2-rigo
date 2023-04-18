f770d0bd2491c74179ed04e2ab989201fec51c89
Ezequiel Rigo <rigoezequiel@gmail.com>
2023-04-18 09:38:36 -0300

    test iniciales

--- test/ar/edu/unq/po2/tp5/ProductoCooperativaTest.java
+++ test/ar/edu/unq/po2/tp5/ProductoCooperativaTest.java
@@ -2,13 +2,34 @@ package ar.edu.unq.po2.tp5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProductoCooperativaTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	private ProductoCooperativa leche;
+	
+	@BeforeEach
+	public void setUp() {
+		leche = new ProductoCooperativa(10.0d,20);
 	}
 
+	@Test
+	void testConstructor() {
+		assertEquals(10.0d,leche.precio);
+		assertEquals(9.0d,leche.getPrecio());
+		assertEquals(20,leche.getCantidadEnStock());
+	}
+	
+	@Test
+	void testGetPrecio() {
+		assertEquals(9.0d,leche.getPrecio());
+	}
+
+	@Test
+	void testGetySetDescuento() {
+		leche.setDescuento(0.5);
+		assertEquals(5.0d,leche.getPrecio());
+		assertEquals(0.5d,leche.getDescuento());
+	}
 }
--- test/ar/edu/unq/po2/tp5/ProductoTest.java
+++ test/ar/edu/unq/po2/tp5/ProductoTest.java
@@ -10,12 +10,36 @@ class ProductoTest {
 	private Producto mermelada;
 	
 	@BeforeEach
-	
+	public void setUp() {
+		mermelada = new Producto(250.5d,50);
+	}
 	
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		assertEquals(250.5d, mermelada.getPrecio());
+		assertEquals(50, mermelada.getCantidadEnStock());
+	}
+	
+	@Test
+	void testSetPrecio() {
+		mermelada.setPrecio(80.8d);
+		assertEquals(80.8d, mermelada.getPrecio());
+	}
+	
+	@Test
+	void testSetCantidadEnStock() {
+		mermelada.setCantidadEnStock(0);
+		assertEquals(0, mermelada.getCantidadEnStock());
+	}
+	
+	@Test
+	void testDisminuirStock() {
+		mermelada.disminuirStock();
+		assertEquals(49, mermelada.getCantidadEnStock());
+		mermelada.setCantidadEnStock(0);
+		mermelada.disminuirStock();
+		assertEquals(-1, mermelada.getCantidadEnStock()); // no debería llamarse este método si el stock está en cero.
 	}
 
 }
